a06224d501b59c82253b7a8a8e8f0d5d5a4ecbc5
Juan Cuéllar <96099514+juan617dtu@users.noreply.github.com>
2023-03-06 19:01:09 -0500

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,26 @@
+print("\n\n\n*********************************")
+print("      MULTIPLICATION TABLES      ")
+print("*********************************")
+print("\n\n___________________________________________________________________________________________________\n\n")
+
+while True:
+    data = input("\n\nENTER A NUMBER FROM 1 TO 12 (0 TO EXIT): ")
+    checkData = data.strip()
+    if checkData.isdigit():
+        num = int(checkData)
+        if num <= 12 and num >= 1:
+            break
+        elif num == 0: exit(0)
+
+print("\n\n ---------------")
+
+i = 0
+
+while i != num:
+    j = i * num
+    print("| {:<2d} X {:<2d} = {:<3d} |".format(i, num, j))
+    i += 1
+
+j = i * num
+print("| {:<2d} X {:<2d} = {:<3d} |".format(i, num, j))
+print(" ---------------\n\n\n")
